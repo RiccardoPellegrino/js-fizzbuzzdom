62691a8997898a9eaa8ce55d40cca671059ced88
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-10 16:19:52 +0200

    piccolo titolo :-)

---
+++ js/script.js
@@ -0,0 +1,42 @@
+"use strict";
+
+let html = '';
+
+const div = document.querySelector("div");
+div.className= div.classList + " ciao ";
+
+for (let i = 1; i <= 100; i++) {
+
+    if (i % 15 == 0) {
+        let elemento = document.createElement('div4');
+        elemento.className = `box--${i}`;
+        elemento.append(`FizzBuzz ${i}`);
+        div.append(elemento);
+        console.log("FizzBuzz");
+    }
+
+    else if (i % 3 == 0) {
+        let elemento = document.createElement('div1');
+        elemento.className = `box--${i}`;
+        elemento.append(`Fizz ${i}`);
+        div.append(elemento);
+        console.log("Fizz");
+    }
+
+    else if (i % 5 == 0) {
+        let elemento = document.createElement('div2');
+        elemento.className = `box--${i}`;
+        elemento.append(`Fizz ${i}`);
+        div.append(elemento);
+        console.log("Buzz");
+    }
+
+    else {
+        let elemento = document.createElement('div3');
+        elemento.className = `box--${i}`;
+        elemento.append(` ${i}`);
+        div.append(elemento);
+        console.log(i)
+    }
+
+};
