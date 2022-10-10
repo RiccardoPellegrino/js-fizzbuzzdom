dc2b2f9225f6f703d125832236def674e3986e33
RiccardoPellegrino <pellegrino.riccardo.90@gmail.com>
2022-10-10 16:24:34 +0200

    modificato buzz e numeri tolta classe inutile ciao

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ for (let i = 1; i <= 100; i++) {
     if (i % 15 == 0) {
         let elemento = document.createElement('div4');
         elemento.className = `box--${i}`;
-        elemento.append(`FizzBuzz ${i}`);
+        elemento.append(`FizzBuzz `);
         div.append(elemento);
         console.log("FizzBuzz");
     }
@@ -18,7 +18,7 @@ for (let i = 1; i <= 100; i++) {
     else if (i % 3 == 0) {
         let elemento = document.createElement('div1');
         elemento.className = `box--${i}`;
-        elemento.append(`Fizz ${i}`);
+        elemento.append(`Fizz `);
         div.append(elemento);
         console.log("Fizz");
     }
@@ -26,7 +26,7 @@ for (let i = 1; i <= 100; i++) {
     else if (i % 5 == 0) {
         let elemento = document.createElement('div2');
         elemento.className = `box--${i}`;
-        elemento.append(`Fizz ${i}`);
+        elemento.append(`Buzz `);
         div.append(elemento);
         console.log("Buzz");
     }
